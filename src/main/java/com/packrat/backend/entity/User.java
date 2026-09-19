81473aab7c2f21c129128ff62355d6dd119efec5
Marco <marco.schoch@gmx.ch>
2026-09-19 21:19:39 +0700

    feat: implement e DevDataInitializer so there is a Dev User for development and only in local mode #5

--- src/main/java/com/packrat/backend/entity/User.java
+++ src/main/java/com/packrat/backend/entity/User.java
@@ -6,13 +6,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity 
 @Getter @Setter 
-@NoArgsConstructor 
+@NoArgsConstructor
+@Table(name = "users") 
 public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.UUID)
