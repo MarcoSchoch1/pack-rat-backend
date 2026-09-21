7401e16f1503df60f188818011d60d04a2668c03
Marco <marco.schoch@gmx.ch>
2026-09-21 20:35:00 +0700

    feat: add a login endpoint for getting a jwt for the userlogin #9

---
+++ src/main/java/com/packrat/backend/dto/LoginResponse.java
@@ -0,0 +1,5 @@
+package com.packrat.backend.dto;
+
+public record LoginResponse(String jwtAccessToken) {
+    
+}
--- src/main/java/com/packrat/backend/security/SecurityConfig.java
+++ src/main/java/com/packrat/backend/security/SecurityConfig.java
@@ -1,15 +1,16 @@
-package com.packrat.backend.config;
+package com.packrat.backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
-public class PasswordConfig {
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
+@Configuration 
+public class SecurityConfig {
+    
+    @Bean 
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    
 }
